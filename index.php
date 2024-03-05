2b44ede2fa6644e202db6d96583382bb9eeddcce
MatteoOliva <oliva.matteo98@outlook.com>
2024-03-05 17:29:16 +0100

    feat: creato la funzione per generare la password

--- index.php
+++ index.php
@@ -1,38 +1,45 @@
 <?php
-// prendere il valore che e' stato inviato.
-// se ce trasformalo in intero e assegnalo altrimenti assegna un valore di default
-$password_length = isset($_GET['password-length']) ? (int) $_GET['password-length'] : 5;   
 
-
-
- // per sapere se il form e' stato inviato
- $form_sent = !empty($_GET);
- // se il form e' stato inviato
- if($form_sent) {
-
+function generate_password($length)
+{
     $generated_password = "";
 
-    for($i=0; $i < $password_length; $i++ ) {
-       
-    // stingra di caratteri
-    $all_chars = range('!', 'þ');
-    
-    // indice casuale con range numero di caratteri nella stringa
-    $rand_char_index = rand(0, count($all_chars) - 1);
-    
-    // carattere randomico della stringa chars nella posizione dell'indice casuale
-    $rand_char = $all_chars[$rand_char_index];
+    for ($i = 0; $i < $length; $i++) {
 
-    // aggiungo alla passoword i caratteri
-    $generated_password .= $rand_char;
-    // stampo password
-};
+        // stinga di caratteri
+        $all_chars = range('!', 'þ');
 
-var_dump($generated_password);
-    
+        // indice casuale con range numero di caratteri nella stringa
+        $rand_char_index = rand(0, count($all_chars) - 1);
 
+        // carattere randomico della stringa chars nella posizione dell'indice casuale
+        $rand_char = $all_chars[$rand_char_index];
+
+        // aggiungo alla passoword i caratteri
+        $generated_password .= $rand_char;
+        // stampo password
+    }
+    ;
+    return $generated_password;
 
 }
+
+// prendere il valore che e' stato inviato.
+// se ce trasformalo in intero e assegnalo altrimenti assegna un valore di default
+$password_length = isset($_GET['password-length']) ? (int) $_GET['password-length'] : 5;
+
+
+
+// per sapere se il form e' stato inviato
+$form_sent = !empty($_GET);
+// se il form e' stato inviato
+if ($form_sent) {
+    // password generata in base alla lunghezza inserita
+    $generate_password = generate_password($password_length);
+    var_dump($generate_password);
+};
+
+
 ?>
 
 
@@ -53,23 +60,24 @@ var_dump($generated_password);
         <div class="card">
             <div class="card-body">
                 <!-- il get e' implicito -->
-        <form class="row">
-            <div class="col-10">
-                <label for="password-length" class="form-label">
-                    Lunghezza Password
-                </label>
-                <input type="number" class="form-control" name="password-length" id="password-length" value="<?= $password_length?>">
-            </div>
-            <div class="col-2 d-flex align-items-end">
-                <button class="btn btn-success w-100">
-                    Genera Password
-                </button>
-            </div>
-        </form>
+                <form class="row">
+                    <div class="col-10">
+                        <label for="password-length" class="form-label">
+                            Lunghezza Password
+                        </label>
+                        <input type="number" class="form-control" name="password-length" id="password-length"
+                            value="<?= $password_length ?>">
+                    </div>
+                    <div class="col-2 d-flex align-items-end">
+                        <button class="btn btn-success w-100">
+                            Genera Password
+                        </button>
+                    </div>
+                </form>
 
             </div>
         </div>
-        
+
     </div>
 
 </body>
